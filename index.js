9c91d5b0b2e0272e490538488e9d93195c61c25d
Rxrotary <bumseokshim@gmail.com>
2019-01-12 15:21:00 +0800

    secondStage

--- index.js
+++ index.js
@@ -1,22 +1,51 @@
-const puppeteer = require('puppeteer')
+const puppeteer = require('puppeteer');
+var stringify = require('csv-stringify');
 
 void (async () => {
-    const browser = await puppeteer.launch({ headless: false });
-    const page = await browser.newPage();
-    await page.goto('https://data.gov.hk/en-datasets/category/finance?order=name&file-content=no');
 
-    while (await page.$('#load-more > a') !== null) {
-        await page.click('#load-more > a');
-        await page.waitFor(3000);
-    };
 
-    await page.waitForSelector('div.dataset-content > h3 > a');
-    const stories = await page.evaluate(() => {
-        const links = Array.from(document.querySelectorAll('div.dataset-content > h3 > a'))
-        return links.map(link => link.href).slice(0)
-    })
-    console.log(stories);
-    await browser.close();
+    var categories = ['city-management', 'climate-and-weather', 'commerce-and-industry',
+        'development', 'education', 'employment-and-labour', 'environment',
+        'finance', 'food', 'health', 'housing', 'it-and-broadcasting',
+        'law-and-security', 'miscellaneous', 'population', 'recreation-and-culture',
+        'social-welfare', 'transport']
+
+    for (i in categories) {
+        const browser = await puppeteer.launch({ headless: false });
+        const page = await browser.newPage();
+        await page.goto(`https://data.gov.hk/en-datasets/category/${categories[i]}?order=name&file-content=no`);
+
+        while (await page.$('#load-more > a') !== null) {
+            await page.click('#load-more > a');
+            await page.waitFor(3000);
+        };
+
+        const info = await page.evaluate(() =>
+            Array.from(document.querySelectorAll('.dataset-item'))
+                .map(link => ({
+                    title: link.querySelector('div.dataset-content > h3 > a').textContent,
+                    notes: link.querySelector('div.dataset-content > div.notes').textContent,
+                    link: link.querySelector('div.dataset-content > h3 > a').href,
+                    format: link.querySelector('div.group-resource-format > ul.dataset-resources.list-unstyled > li > span > a').textContent
+                }))
+                .slice(0, 3)
+        )
+        console.log(info);
+        await browser.close(); 
+        
+        var input = [] 
+        var line = logLine.split(",");
+        input.push(line);
+        
+        await stringify(input, function(err, info){
+          fs.writeFile(`${categoreis[i]}.csv`, info);
+
+
+         
+        });
+    }
+
+
 })();
 
 
